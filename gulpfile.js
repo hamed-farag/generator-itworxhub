806697f161fbcb4fe2054bd6df8d2ca0919eee4f
Hamed Farag <hamed.farag.2009@gmail.com>
2018-03-23 13:49:13 +0200

    Add  coveralls  configuration.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
+var coveralls = require('gulp-coveralls');
+
+gulp.src('test/coverage/**/lcov.info')
+  .pipe(coveralls());
 
 gulp.task('test', function() {
   return gulp.src(['test/*.test.js'], { read: false })
