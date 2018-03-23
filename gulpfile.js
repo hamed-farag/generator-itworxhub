7078f30e32ea195f821f0dcb565524559b286f05
Hamed Farag <hamed.farag.2009@gmail.com>
2018-03-24 01:12:33 +0200

    Update Gulp Configuraion to run coveralls

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,15 @@ var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 var coveralls = require('gulp-coveralls');
 
-gulp.src('test/coverage/**/lcov.info')
-  .pipe(coveralls());
+gulp.task('coveralls', function () {
+  return gulp.src('test/coverage/**/lcov.info')
+    .pipe(coveralls());
+});
 
-gulp.task('test', function() {
-  return gulp.src(['test/*.test.js'], { read: false })
+gulp.task('test', function () {
+  return gulp.src(['test/*.test.js'], {
+      read: false
+    })
     .pipe(mocha({
       reporter: 'spec'
     }));
