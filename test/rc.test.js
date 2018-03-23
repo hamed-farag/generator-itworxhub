eb234dc8dcaefb9bf744b88ce5743895b1ede5ae
Hamed Farag <hamed.farag.2009@gmail.com>
2018-03-23 13:33:10 +0200

    Update Test Cases

---
+++ test/rc.test.js
@@ -0,0 +1,51 @@
+var path = require('path');
+var yoTestHelpers = require('yeoman-test');
+var yoTestAssert = require('yeoman-assert');
+var {
+    capitalizeFirstLetter
+} = require('../helpers');
+
+describe('itworxhub:rc', function () {
+    const componentName = 'reactComponent';
+    const templatePath = path.join(__dirname, '../generators/rc');
+
+    describe('Create Style Sheet', function () {
+        before(function (done) {
+            yoTestHelpers.run(templatePath)
+                .withArguments([componentName])
+                .withPrompts({
+                    hasStyleSheet: true
+                })
+                .on('end', done);
+        });
+
+        it('Inject componentName Argument', function () {
+            yoTestAssert.fileContent(
+                `${capitalizeFirstLetter(componentName)}/styles.scss`,
+                `.${capitalizeFirstLetter(componentName)}Container {}`
+            );
+            yoTestAssert.fileContent(
+                `${capitalizeFirstLetter(componentName)}/index.jsx`,
+                `<div className='${capitalizeFirstLetter(componentName)}Container'>`
+            );
+        });
+    });
+
+    describe('Without Create Style Sheet', function () {
+        before(function (done) {
+            yoTestHelpers.run(templatePath)
+                .withArguments([componentName])
+                .withPrompts({
+                    hasStyleSheet: false
+                })
+                .on('end', done);
+        });
+
+        it('Inject componentName Argument', function () {
+            yoTestAssert.fileContent(
+                `${capitalizeFirstLetter(componentName)}/index.jsx`,
+                `<div className='${capitalizeFirstLetter(componentName)}Container'>`
+            );
+        });
+    });
+});
--- test/rsc.test.js
+++ test/rsc.test.js
@@ -2,26 +2,47 @@ var path = require('path');
 var yoTestHelpers = require('yeoman-test');
 var yoTestAssert = require('yeoman-assert');
 
-
 describe('itworxhub:rsc', function () {
 	const componentName = 'reactStatelessComponent';
-	before(function (done) {
-		yoTestHelpers.run(path.join(__dirname, '../generators/rsc'))
-			.withArguments([componentName])
-			.withPrompts({
-				hasStyleSheet: true
-			})
-			.on('end', done);
+	const templatePath = path.join(__dirname, '../generators/rsc');
+
+	describe('Create Style Sheet', function () {
+		before(function (done) {
+			yoTestHelpers.run(templatePath)
+				.withArguments([componentName])
+				.withPrompts({
+					hasStyleSheet: true
+				})
+				.on('end', done);
+		});
+
+		it('Inject componentName Argument', function () {
+			yoTestAssert.fileContent(
+				`${componentName}/styles.scss`,
+				`.${componentName}Container {}`
+			);
+			yoTestAssert.fileContent(
+				`${componentName}/index.jsx`,
+				`<div className='${componentName}Container'>`
+			);
+		});
 	});
 
-	it('Inject componentName Argument', function () {
-		yoTestAssert.fileContent(
-			`${componentName}/styles.scss`,
-			`.${componentName}Container {}`
-		);
-		yoTestAssert.fileContent(
-			`${componentName}/index.jsx`,
-			`<div className='${componentName}Container'>`
-		);
+	describe('Without Create Style Sheet', function () {
+		before(function (done) {
+			yoTestHelpers.run(templatePath)
+				.withArguments([componentName])
+				.withPrompts({
+					hasStyleSheet: false
+				})
+				.on('end', done);
+		});
+
+		it('Inject componentName Argument', function () {
+			yoTestAssert.fileContent(
+				`${componentName}/index.jsx`,
+				`<div className='${componentName}Container'>`
+			);
+		});
 	});
 });
